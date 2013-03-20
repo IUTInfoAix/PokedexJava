932774e4e2040ab5168931275a2cea5391fa9a7a
Sébastien NEDJAR <nedseb@goldaire.com>
2013-03-20 16:17:58 +0100

    Refactoring des test et injection du mechanizer dans Extractor

--- src/main/java/fr/univaix/iut/pokebattle/Extractor.java
+++ src/main/java/fr/univaix/iut/pokebattle/Extractor.java
@@ -8,12 +8,22 @@ import java.util.List;
 
 public class Extractor {
     public final static String LIST_ATTACK_URL = "http://www.pokepedia.fr/index.php/Liste_des_attaques";
+    MechanizeAgent agent;
+
+    Extractor(MechanizeAgent agent) {
+        this.agent = agent;
+    }
 
     public Attack ExtractAttack(String nomAttack) {
-        Attack attack = new Attack(nomAttack);
-        if (nomAttack.equals("Charge") || nomAttack.equals("Trempette"))
-            return attack;
-        return null;
+
+        Document page = agent.get(Extractor.LIST_ATTACK_URL);
+        Node baliseAttack = page.getRoot().find("a[title='" + nomAttack + "']");
+
+        if (baliseAttack.getName() == null)
+            return null;
+
+        Node tr = baliseAttack.getParent().getParent();
+        return ExtractAttack(tr);
     }
 
     public Attack ExtractAttack(Node tr) {
@@ -25,7 +35,7 @@ public class Extractor {
         MechanizeAgent agent = new MechanizeAgent();
         Document page = agent.get(Extractor.LIST_ATTACK_URL);
         List<? extends Node> tables = page.getRoot().findAll("table.wikitable");
-        Extractor extractor = new Extractor();
+        Extractor extractor = new Extractor(agent);
         for (Node table : tables) {
             List<? extends Node> trs = table.findAll("tr:nth-child(n+2)");
             for (Node tr : trs) {
--- src/test/java/fr/univaix/iut/pokebattle/ExtractorTest.java
+++ src/test/java/fr/univaix/iut/pokebattle/ExtractorTest.java
@@ -3,53 +3,72 @@ package fr.univaix.iut.pokebattle;
 
 import com.gistlabs.mechanize.document.Document;
 import com.gistlabs.mechanize.document.node.Node;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class ExtractorTest {
-    MechanizeMock agent = new MechanizeMock();
-    final static String trempetteHtml = "<table><tr> \n" +
+
+    final static String trempetteHtml = "<tr> \n" +
             " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Trempette\" title=\"Trempette\">Trempette</a> </td> \n" +
             " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Normal_(type)\" title=\"Normal (type)\"><img alt=\"normal\" src=\"/images/9/91/Type_miniat_normal.png\" width=\"32\" height=\"14\" /></a> </td> \n" +
             " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Cat%C3%A9gories_d%27attaque\" title=\"Cat&eacute;gories d'attaque\"><img alt=\"Cat&eacute;gorie miniat statut.gif\" src=\"/images/6/66/Cat%C3%A9gorie_miniat_statut.gif\" width=\"32\" height=\"14\" /></a> </td> \n" +
             " <td style=\"vertical-align:middle\"> — </td> \n" +
             " <td style=\"vertical-align:middle\"> — </td> \n" +
             " <td style=\"vertical-align:middle\"> 40 </td>\n" +
-            "</tr></table>";
+            "</tr>";
+    final static String ChargeHtml = "<tr> \n" +
+            " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Charge\" title=\"Charge\">Charge</a> </td> \n" +
+            " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Normal_(type)\" title=\"Normal (type)\"><img alt=\"normal\" src=\"/images/9/91/Type_miniat_normal.png\" width=\"32\" height=\"14\" /></a> </td> \n" +
+            " <td style=\"vertical-align:middle\"> <a href=\"/index.php/Cat%C3%A9gories_d%27attaque\" title=\"Cat&eacute;gories d'attaque\"><img alt=\"Cat&eacute;gorie miniat physique.gif\" src=\"/images/c/c4/Cat%C3%A9gorie_miniat_physique.gif\" width=\"32\" height=\"14\" /></a> </td> \n" +
+            " <td style=\"vertical-align:middle\"> 50<span class=\"explain\" title=\"35 de la premi&egrave;re &agrave; la quatri&egrave;me g&eacute;n&eacute;ration incluse\">*</span> </td> \n" +
+            " <td style=\"vertical-align:middle\"> 100%<span class=\"explain\" title=\"95% de la premi&egrave;re &agrave; la quatri&egrave;me g&eacute;n&eacute;ration incluse\">*</span> </td> \n" +
+            " <td style=\"vertical-align:middle\"> 30 </td>\n" +
+            "</tr>";
+
+    private Extractor extractor;
+    private MechanizeMock agent;
+
+    @Before
+    public void setUp() {
+        agent = new MechanizeMock();
+        agent.addPageRequest(Extractor.LIST_ATTACK_URL, newHtml("Test Page", newTable(trempetteHtml + ChargeHtml)));
+        extractor = new Extractor(agent);
+    }
 
     @Test
     public void testExtractAttackGivenKebabShouldReturnNull() {
-        Extractor extractor = new Extractor();
+
         assertThat(extractor.ExtractAttack("Kebab")).isNull();
     }
 
     @Test
     public void testExtractAttackGivenChargeShouldReturnAttackCharge() {
-        Extractor extractor = new Extractor();
+
         Attack attack = extractor.ExtractAttack("Charge");
         assertThat(attack.getName()).isEqualTo("Charge");
     }
 
     @Test
     public void testExtractAttackGivenTrempetteShouldReturnAttackTrempette() {
-        Extractor extractor = new Extractor();
         Attack attack = extractor.ExtractAttack("Trempette");
         assertThat(attack.getName()).isEqualTo("Trempette");
     }
 
     @Test
     public void testExtractAttackGivenNodeTrempetteShouldReturnTrempette() {
-        agent.addPageRequest(Extractor.LIST_ATTACK_URL, newHtml("Test Page", trempetteHtml));
         Document page = agent.get(Extractor.LIST_ATTACK_URL);
-
         Node tr = page.getRoot().find("tr");
-        Extractor extractor = new Extractor();
         Attack attack = extractor.ExtractAttack(tr);
         assertThat(attack.getName()).isEqualTo("Trempette");
     }
 
     private String newHtml(String title, String bodyHtml) {
         return "<html><head><title>" + title + "</title></head><body>" + bodyHtml + "</body></html>";
+    }
+
+    private String newTable(String content) {
+        return "<table>\n" + content + "\n</table>";
     }
 }
