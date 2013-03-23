fc709e6a29bb6759ff71bd262e7dd7096d7027ec
Sébastien NEDJAR <nedseb@goldaire.com>
2013-03-23 16:41:52 +0100

    Nettoyage de la classe extractor pour la rendre plus lisible

--- src/main/java/fr/univaix/iut/pokebattle/extractor/Extractor.java
+++ src/main/java/fr/univaix/iut/pokebattle/extractor/Extractor.java
@@ -3,39 +3,39 @@ package fr.univaix.iut.pokebattle.extractor;
 import com.gistlabs.mechanize.MechanizeAgent;
 import com.gistlabs.mechanize.document.Document;
 import com.gistlabs.mechanize.document.node.Node;
+import com.google.common.collect.Lists;
 import fr.univaix.iut.pokebattle.parser.AttackParser;
 import fr.univaix.iut.pokebattle.pokemon.Attack;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Extractor {
-    private final AttackParser attackExtractor;
-    private final Document page;
+    private final AttackParser attackParser;
+    private final Node root;
 
     public Extractor(MechanizeAgent agent) {
-        this.page = agent.get(AttackParser.LIST_ATTACK_URL);
-        this.attackExtractor = new AttackParser();
+        Document page = agent.get(AttackParser.LIST_ATTACK_URL);
+        this.root = page.getRoot();
+        this.attackParser = new AttackParser();
     }
 
     public Attack ExtractAttack(String nomAttack) {
-        Node anchorAttackName = page.getRoot().find("a[title^='" + nomAttack + "']");
+        Node anchorAttackName = root.find("a[title^='" + nomAttack + "']");
 
         if (anchorAttackName.getName() == null)
             return null;
 
-        Node tr = anchorAttackName.getParent().getParent();
+        Node tr = getEnclosingTableRow(anchorAttackName);
         return ExtractAttack(tr);
     }
 
-    private Attack ExtractAttack(Node tr) {
-        List<? extends Node> tds = tr.findAll("td");
-        return attackExtractor.parseAttack(tds);
+    private Node getEnclosingTableRow(Node anchorAttackName) {
+        return anchorAttackName.getParent().getParent();
     }
 
     public List<Attack> ExtractAttacks() {
-        List<Attack> attacks = new ArrayList<Attack>();
-        List<? extends Node> tables = getAttackTables();
+        List<Attack> attacks = Lists.newArrayList();
+        List<? extends Node> tables = getTables(root);
         for (Node table : tables) {
             List<? extends Node> trs = getTableRowsExceptHeader(table);
             for (Node tr : trs) {
@@ -47,12 +47,21 @@ public class Extractor {
         return attacks;
     }
 
+    private Attack ExtractAttack(Node tr) {
+        List<? extends Node> tds = getTableCells(tr);
+        return attackParser.parseAttack(tds);
+    }
+
+    private List<? extends Node> getTableCells(Node tr) {
+        return tr.findAll("td");
+    }
+
     private List<? extends Node> getTableRowsExceptHeader(Node table) {
         return table.findAll("tr:nth-child(n+2)");
     }
 
-    private List<? extends Node> getAttackTables() {
-        return page.getRoot().findAll("table");
+    private List<? extends Node> getTables(Node root) {
+        return root.findAll("table");
     }
 
     private static boolean isValidAttackRow(Node tr) {
